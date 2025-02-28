7407a6e8d6a98c3c18e355fe4333171deb0635e5
darkros <madsmario@gmail.com>
2025-02-28 16:57:30 +0100

    fixed error in sda

--- src/sda/main.py
+++ src/sda/main.py
@@ -4,13 +4,6 @@ from helper.util import print_result, sda_profiling
 
 def main(settings: dict[str, int], data: DataFrame):
     profiles = sda_profiling(settings, data)
-
-    for snd_port, profile in profiles.items():
-        total = sum(profile.values())
-        for rcv_port, score in profile.items():
-            profiles[snd_port][rcv_port] = score / total
-
-        profiles[snd_port] = dict(sorted(profile.items(), key=lambda item: item[1]))
-
-    print_result(profiles, settings)
+    
+    print_result(profiles.div(profiles.sum(axis=1), axis=0).to_dict(), settings)
 
