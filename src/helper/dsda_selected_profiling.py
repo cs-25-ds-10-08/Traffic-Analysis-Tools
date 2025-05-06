31a4f797e4d31f6e96ed8821a840bd39ebf7dd19
darkros <madsmario@gmail.com>
2025-05-06 09:05:18 +0200

    The attack works utilizing a clustering method

--- src/helper/dsda_selected_profiling.py
+++ src/helper/dsda_selected_profiling.py
@@ -1,5 +1,5 @@
 from collections import Counter, defaultdict
-from typing import TypedDict
+from typing import Optional, TypedDict
 from pandas import DataFrame
 from tqdm import tqdm
 
@@ -11,21 +11,18 @@ RemaningTime = float
 class Events(TypedDict):
     id: list[Identifier]
     time: list[float]
+    size: Optional[list[int]]
 
 
 def sda_selected_profiling(settings: DenimSettings, data: DataFrame) -> DataFrame:
     server = settings["server"]
 
-    burst_events: Events = {"id": [], "time": []}
-    burst_events_candidates: defaultdict[Identifier, list[RemaningTime]] = defaultdict(list[RemaningTime])
-    receivers: Events = {"id": [], "time": []}
-
-    prev_time: float = data.iloc[0].Time
+    burst_events: Events = {"id": [], "time": [], "size": []}
+    chunks_by_id: dict[str, list[list[tuple[str, float]]]] = {}
+    receivers: Events = {"id": [], "time": [], "size": None}
 
     for row in tqdm(data.itertuples(), total=data.shape[0], desc="Preparing senders and receivers"):
         time: float = row.Time  # type: ignore
-        elapsed_time = time - prev_time
-        burst_events_candidates = _update_candidate_buffer(burst_events_candidates, elapsed_time)
 
         src_dst = get_src_and_dst(row)
         sender = src_dst["src"]
@@ -35,30 +32,41 @@ def sda_selected_profiling(settings: DenimSettings, data: DataFrame) -> DataFram
             receivers["id"].append(receiver)
             receivers["time"].append(time)
         else:
-            burst_events_candidates[sender].append(settings["dt"])
-            _mayber_append(
-                burst_events,
-                burst_events_candidates,
-                sender,
-                settings["n"],
-                time,
-            )
+            if sender not in chunks_by_id:
+                # First time seeing this identifier
+                chunks_by_id[sender] = [[(sender, time)]]
+            else:
+                last_chunk = chunks_by_id[sender][-1]
+                # Compare with last timestamp in the last chunk
+                if time - last_chunk[-1][1] <= settings["dt"]:
+                    last_chunk.append((sender, time))
+                else:
+                    # Start a new chunk
+                    if burst_events["size"] is not None and len(last_chunk) >= 3:
+                        burst_events["id"].append(sender)
+                        burst_events["time"].append(time)
+                        burst_events["size"].append(len(last_chunk))
+                        chunks_by_id[sender].pop()
+                    chunks_by_id[sender].append([(sender, time)])
 
-        prev_time = time
+    if burst_events["size"] is not None:
+        combined = list(zip(burst_events["id"], burst_events["time"], burst_events["size"]))
+        sorted_combined = sorted(combined, key=lambda x: x[1])
+        burst_events["id"], burst_events["time"], burst_events["size"] = map(list, zip(*sorted_combined))
 
     print(Counter(burst_events["id"]))
 
-    profiles = _make_profiles(DataFrame(burst_events), DataFrame(receivers), settings["n"])
+    profiles = _make_profiles(DataFrame(burst_events), DataFrame(receivers))
 
     return DataFrame.from_dict(profiles).fillna(0)
 
 
-def _make_profiles(burst_events: DataFrame, receivers: DataFrame, n: int) -> dict[Identifier, Profile]:
+def _make_profiles(burst_events: DataFrame, receivers: DataFrame) -> dict[Identifier, Profile]:
     profiles: dict[Identifier, Profile] = defaultdict(lambda: defaultdict(float))
 
     for row in tqdm(burst_events.itertuples(), total=burst_events.shape[0], desc="Making profiles"):
         next = -1
-        for j in range(row.Index + 1, burst_events.shape[0]):
+        for j in range(row.Index + 1, burst_events.shape[0]):  # type: ignore
             if burst_events.loc[row.Index].id == burst_events.iloc[j].id:
                 next = j
                 burst_events_slice = burst_events.iloc[
@@ -81,23 +89,25 @@ def _make_profiles(burst_events: DataFrame, receivers: DataFrame, n: int) -> dic
                 potential_receivers = _filter_potential_receivers(
                     burst_events_slice,
                     pr,
-                    row.id,
-                    n,
+                    row.id,  # type: ignore
+                    row.size,  # type: ignore
                 )
+                # print(f"1: {potential_receivers1}")
+                # print(f"2: {potential_receivers2}")
                 if potential_receivers.empty:
                     continue
 
                 amount = potential_receivers.shape[0] - Counter(potential_receivers["id"])[row.id]
                 for potential_receiver in potential_receivers.itertuples():
                     if row.id != potential_receiver.id:
-                        profiles[row.id][potential_receiver.id] += 1 / amount
+                        profiles[row.id][potential_receiver.id] += 1 / amount  # type: ignore
 
                 break
 
     return profiles
 
 
-def _filter_potential_receivers(burst_events: DataFrame, receivers: DataFrame, id: Identifier, n: int) -> DataFrame:
+def _filter_potential_receiversv1(burst_events: DataFrame, receivers: DataFrame, id: Identifier, n: int) -> DataFrame:
     count = 0
     counts = defaultdict(int)
     ids = burst_events.id.values
@@ -113,26 +123,25 @@ def _filter_potential_receivers(burst_events: DataFrame, receivers: DataFrame, i
     return burst_events[[counts[row.id] >= n for row in burst_events.itertuples()]]
 
 
-def _update_candidate_buffer(
-    sender_buffer: defaultdict[Identifier, list[RemaningTime]], elapsed_time: float
-) -> defaultdict[Identifier, list[RemaningTime]]:
-    for sender, times in sender_buffer.items():
-        sender_buffer[sender] = [
-            remaning_time - elapsed_time for remaning_time in times if remaning_time - elapsed_time > 0
-        ]
+def _filter_potential_receivers(burst_events: DataFrame, receivers: DataFrame, id: Identifier, n: int) -> DataFrame:
+    count = 0
+    counts = {}
+    i = burst_events.shape[0] - 1
 
-    return sender_buffer
+    for receiver in receivers.iloc[::-1].itertuples():
+        while i >= 0:
+            burst_event = burst_events.iloc[i]
+            if receiver.time < burst_event.time:
+                if burst_event["size"] <= count and burst_event.id not in counts:
+                    counts[burst_event.id] = 0
+                i -= 1
+            else:
+                break
 
+        if receiver.id == id:
+            count += 1
+            continue
+        if receiver.id in counts:
+            counts[receiver.id] += 1
 
-def _mayber_append(
-    burst_events: Events,
-    burst_events_candidates: defaultdict[Identifier, list[RemaningTime]],
-    sender: Identifier,
-    n: int,
-    time: float,
-):
-    if len(burst_events_candidates[sender]) == n:
-        # print(f"{sender}: {[time[1] for time in burst_events_candidates[sender]]} \n")
-        burst_events["id"].append(sender)
-        burst_events["time"].append(time)
-        burst_events_candidates.pop(sender)
+    return burst_events[[row.id in counts and counts[row.id] >= n for row in burst_events.itertuples()]]
