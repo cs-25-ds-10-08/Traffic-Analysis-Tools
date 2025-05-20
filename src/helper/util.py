73b2a319b38101eb3d4c01ef3f43a1592cbfbe29
darkros <madsmario@gmail.com>
2025-05-20 09:14:47 +0200

    Changed pygmtools to use the latest, due to issues with bdist_wheel and setup tools

--- src/helper/util.py
+++ src/helper/util.py
@@ -21,7 +21,7 @@ Profile = dict[Identifier, float]
 LOCALHOST = ["127.0.0.1", "::1"]
 
 
-def print_result(profiles: dict[Identifier, Profile], settings: Settings):
+def print_result(profiles: dict[Identifier, Profile], settings: CommonSettings):
     result = max(profiles[settings["target"]].items(), key=lambda x: x[1])
     print(
         f"Target: {settings['target']}\nActual: {settings['actual']}\nMost likely: {result[0]}\nWith propability: {round(result[1] * 100, 2)}%"
--- src/sda/main.py
+++ src/sda/main.py
@@ -7,4 +7,4 @@ from helper.util import Settings, print_result
 def main(settings: Settings, data: DataFrame, profiler: Callable[[Settings, DataFrame], DataFrame]):
     profiles = profiler(settings, data)
 
-    print_result(profiles.div(profiles.sum(axis=1), axis=0).to_dict(), settings)
+    print_result(profiles.div(profiles.sum(axis=1), axis=0).to_dict(orient="index"), settings)  # type: ignore
